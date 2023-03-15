880a76a604d2a49ec70151a6c85751623194dcac
JPonceLunaP <jponcelunapizarro@gmail.com>
2023-03-15 16:28:15 -0400

    Prueba

---
+++ Variables.js
@@ -0,0 +1,17 @@
+//String
+var bootcamp = "Henry";
+
+// Numeros
+var numeroEntero = 17;
+var numeroNegativo = -4;
+var numeroDecimal = 11.75;
+
+//Booleanos
+var estoyAburrido = false;
+var estoyEntendiendo = true;
+
+//Undefined
+var cajon;
+
+//Null
+var sinNada = null;
