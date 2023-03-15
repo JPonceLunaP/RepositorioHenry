5204e1d6f47e02d9583ead600cb190db78a62fc2
JPonceLunaP <jponcelunapizarro@gmail.com>
2023-03-15 16:45:36 -0400

    Prueba 1.1

--- Variables.js
+++ Variables.js
@@ -16,4 +16,32 @@ var cajon;
 //Null
 var sinNada = null;
 
-console.log(cajon1);
+//console.log(cajon1);
+
+//Operadores
+    //Suma
+var num1 = 3;
+var num2 = 4;
+
+var suma = num1 + num2;
+
+console.log("Suma:", suma);
+
+    //Resta
+var res = num1 - num2;
+console.log("Resta:", res);
+
+    //Multiplicacion
+var mul = num1*num2;
+console.log(mul);
+
+    //Division
+var div = num1/num2;
+console.log(div);
+
+    //Modulo o resto
+var mod = num1%num2;
+console.log(mod);
+
+    //Precedencia de operadores
+console.log(3+2*5-8);
