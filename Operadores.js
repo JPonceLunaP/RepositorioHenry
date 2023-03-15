72b33242ff7584294078f264695cb3cd31d25cca
JPonceLunaP <jponcelunapizarro@gmail.com>
2023-03-15 16:54:10 -0400

    Prueba 1.0

---
+++ Operadores.js
@@ -0,0 +1,34 @@
+//Operadores
+    //Suma
+    var num1 = 3;
+    var num2 = 4;
+    
+    var suma = num1 + num2;
+    
+    console.log("Suma:", suma);
+    
+        //Resta
+    var res = num1 - num2;
+    console.log("Resta:", res);
+    
+        //Multiplicacion
+    var mul = num1*num2;
+    console.log("Multiplicacion:", mul);
+    
+        //Division
+    var div = num1/num2;
+    console.log("Division:", div);
+    
+        //Modulo o resto
+    var mod = num1%num2;
+    console.log("Modulo:", mod);
+    
+        //Precedencia de operadores
+    console.log(3+2*5-8);
+    
+        //Suma de strings
+    var nombre = "Javier";
+    var saludo = "Hola";
+    var saludar = saludo + " " + nombre;
+    
+    console.log(saludar);
