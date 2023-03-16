71a69f15f4f1a970e0700d4c59bda93aa0913941
JPonceLunaP <jponcelunapizarro@gmail.com>
2023-03-16 14:46:58 -0400

    Prueba 1.0

---
+++ Logicos.js
@@ -0,0 +1,15 @@
+// Negación --NOT
+
+//Conjunsción -- AND -- &&
+function mayorYMenor (num){
+    if(num > 5 && num < 10){
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+mayorYMenor(2);
+mayorYMenor(7);
+mayorYMenor(11);
+// Disyunción -- OR
