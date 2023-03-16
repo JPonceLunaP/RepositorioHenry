a1f4fbf9fbbad20d670c9105e89e7bf559f61e1c
JPonceLunaP <jponcelunapizarro@gmail.com>
2023-03-16 15:17:21 -0400

    Prueba 1.0

---
+++ Bucles.js
@@ -0,0 +1,16 @@
+// For
+var suma = 0;
+for (var i = 0; i < 5; i++){
+    suma = suma + i;
+    console.log("Variable i: ", i);
+    console.log("Variable suma: ", suma);
+}
+
+console.log("Variable suma: ", suma);
+
+// While
+var sum = 0;
+while (sum < 3){
+    sum = sum + 1;
+    console.log(sum);
+}
