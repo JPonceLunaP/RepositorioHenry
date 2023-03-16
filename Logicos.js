d6b9315a0a7304c1cc615440d358fdfb56cf7224
JPonceLunaP <jponcelunapizarro@gmail.com>
2023-03-16 14:59:31 -0400

    Prueba 1.2

--- Logicos.js
+++ Logicos.js
@@ -35,3 +35,18 @@ operadorOR("H");
 operadorOR("hen");
 operadorOR("Henry");
 operadorOR("C");
+
+
+
+function condicionCompleja (num){
+    if(num > 9 && num % 2 === 0 || num === 3){
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+condicionCompleja(10);
+condicionCompleja(6);
+condicionCompleja(3);
+condicionCompleja(11);
