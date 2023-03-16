6a47a61e49e14cdee0fae220781619a43655a5a7
JPonceLunaP <jponcelunapizarro@gmail.com>
2023-03-16 14:56:19 -0400

    Prueba 1.1

--- Logicos.js
+++ Logicos.js
@@ -1,8 +1,17 @@
-// Negación --NOT
+// Negación --NOT-- !
+function negacion (permiso){
+    if (permiso !== true){
+        console.log("No tiene permiso");
+    } else {
+        console.log("Tiene permiso");
+    }
+}
 
+negacion(true);
+negacion(false);
 //Conjunsción -- AND -- &&
 function mayorYMenor (num){
-    if(num > 5 && num < 10){
+    if(num > 5 && num < 10 && num % 2 === 0){
         console.log(true);
     } else {
         console.log(false);
@@ -11,5 +20,18 @@ function mayorYMenor (num){
 
 mayorYMenor(2);
 mayorYMenor(7);
+mayorYMenor(8);
 mayorYMenor(11);
-// Disyunción -- OR
+// Disyunción -- OR -- ||
+function operadorOR (str){
+    if (str === "Henry" || str.length < 2){
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+operadorOR("H");
+operadorOR("hen");
+operadorOR("Henry");
+operadorOR("C");
