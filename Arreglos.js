de6e7f015ceba7bc60f1ca5642406d399e9a444e
JPonceLunaP <jponcelunapizarro@gmail.com>
2023-03-18 17:33:08 -0400

    Prueba 1.0

---
+++ Arreglos.js
@@ -0,0 +1,16 @@
+var listaDeNombres = [];
+
+listaDeNombres[3] = "Juan";
+
+listaDeNombres[1] = "Jose";
+
+console.log(listaDeNombres);
+
+var elementoDelAray = listaDeNombres[1];
+
+console.log(elementoDelAray);
+
+
+//Metodo lenght
+
+console.log(listaDeNombres.length);
