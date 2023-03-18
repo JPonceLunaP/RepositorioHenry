5009b9b3fa9ae6fc27e6027616409ce31e454ae6
JPonceLunaP <jponcelunapizarro@gmail.com>
2023-03-18 17:46:57 -0400

    Prueba 1.2

--- Arreglos.js
+++ Arreglos.js
@@ -18,3 +18,46 @@ console.log(listaDeNombres.length);
 // Lenght
 var nombres = ['Matias', 'Maria', 'Diego', 'Rosa'];
 console.log(nombres.length);
+
+//Agregar elementos -- Push y Unshift
+
+var colores = ["rojo", "azul"];
+
+console.log(colores);
+
+colores.push("amarillo");
+
+console.log(colores);
+
+colores.unshift("verde");
+
+console.log(colores);
+
+
+//eliminar ultimo elemento -- Pop
+
+colores.pop();
+
+console.log(colores);
+
+//eliminar el primer elemento -- Shift
+
+colores.shift();
+
+console.log(colores);
+
+//para saber si incluye un elemento -- Includes
+
+var pintores = ["Picasso", "Velazquez", "Van Gogh", "Dali"];
+var incluyePintor = pintores.includes("Dali");
+console.log(incluyePintor);
+
+var incluyePintor = pintores.includes("Monet");
+console.log(incluyePintor);
+
+// para saber si todos los elementos cumplen con una condicion -- Every
+var numeros = [6,7,8,9];
+var  mayorQueCinco = numeros.every((num) => {
+    return num > 5;
+});
+console.log(mayorQueCinco);
