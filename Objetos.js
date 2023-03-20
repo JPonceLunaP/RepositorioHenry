fcc95aab0e04ab3cac6e5b4a7fef6492dee6c41b
JPonceLunaP <jponcelunapizarro@gmail.com>
2023-03-19 21:33:53 -0400

    Pruebas 1.1

--- Objetos.js
+++ Objetos.js
@@ -10,3 +10,32 @@ var persona =  {
     edad: 26,
     estudios: {esProgramador: true},
 };
+
+//Trabajar con objetos
+    //Dot-Notation -- Notacion por punto
+
+console.log(persona.edad);
+persona.nombre = "Martin";
+console.log(persona.nombre);
+persona.edad = 32;
+console.log(persona.edad);
+
+
+var autos = {};
+
+autos.marcas = ["Ford", "Audi", "Ferrari"];
+
+console.log(autos);
+
+//Borrar propiedades
+
+delete autos.marcas;
+console.log(autos);
+
+var misFunciones = {
+    saludar: function(){  
+        console.log("Hola");
+    },
+};
+
+misFunciones.saludar();
