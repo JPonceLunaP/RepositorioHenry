f7f9007db045f943a47d16607a1239f39a2a9132
JPonceLunaP <jponcelunapizarro@gmail.com>
2023-03-19 21:25:29 -0400

    Prueba 1.0

---
+++ Objetos.js
@@ -0,0 +1,12 @@
+// objetos
+
+var deportes = {
+    conBalon:["Footbol", "Basket", "Rugby"],
+    sinBalon:["Boxeo", "Surf", "Trekkin"],
+};
+
+var persona =  {
+    nombre: "Lucas",
+    edad: 26,
+    estudios: {esProgramador: true},
+};
