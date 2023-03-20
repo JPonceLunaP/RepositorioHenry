2c3a3ab040e9292f852b6c6d8bb8be53e97e1d4e
JPonceLunaP <jponcelunapizarro@gmail.com>
2023-03-19 21:55:57 -0400

    Prueba 1.3

--- Objetos.js
+++ Objetos.js
@@ -57,3 +57,37 @@ var diferenciaDeNotaciones = function(propUno, propDos) {
 
 diferenciaDeNotaciones ("saludable", "noSaludable");
 console.log(comidas);
+
+//Objetos avanzados
+    //hasOwnProperty -- Para saber si tiene una propiedad espesifica
+
+var libro = {autor: "Borges", genero: "Policial", año: 1990};
+
+var tienePropiedad = libro.hasOwnProperty("autor");
+console.log(tienePropiedad);
+
+    //keys -- devuelve un arreglo con los nombres de las propiedades
+var todasLasPropiedades = Object.keys(libro);
+console.log(todasLasPropiedades);
+
+    //for in -- bucle para recorrer las propiedades
+
+var mundo = {continentes: 7, paises: 195, oceanos: 5};
+for (var prop in mundo){
+    console.log("Esta es la propiedad:" + prop);
+    console.log("Este es el valor: " + mundo[prop]);
+};
+
+    // this -- hacemos referencia a un contexto
+var mascota = {
+    animal: "Perro",
+    raza: "Ovejero Aleman",
+    amistoso: true,
+    dueña: "Maria Lopez",
+    info: function() {
+        console.log("mi perro es un " + this.raza);
+    },
+};
+
+mascota.info();
+
