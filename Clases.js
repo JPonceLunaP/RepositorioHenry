01b0bdd1518dd9c40093e90d88a98f0580990493
JPonceLunaP <jponcelunapizarro@gmail.com>
2023-03-20 21:13:39 -0400

    Prueba 1.1

--- Clases.js
+++ Clases.js
@@ -58,3 +58,37 @@ var brasil = new Footbol("Pele");
 
 argentina.obtenerNombre();
 brasil.obtenerNombre();
+
+//Prototivo
+
+Array.prototype.mayoresQueTres = function (){
+    var arregloModificado = [];
+    for (i = 0; i < this.length; i++){
+        if(this[i] > 3){
+            arregloModificado.push(false);
+        } else{
+            arregloModificado.push(this[i]);
+        }
+    }
+    return arregloModificado;
+}
+
+var arreglo = [1,2,3,4,5];
+var nuevoArreglo = arreglo.mayoresQueTres();
+
+console.log(nuevoArreglo);
+
+
+class LatinoAmerica {
+    constructor(){
+       this.paises = []; 
+    }
+}
+
+LatinoAmerica.prototype.agregarPais = function(pais){
+    this.paises.push(pais);
+}
+
+var continente = new LatinoAmerica();
+continente.agregarPais("Mexico");
+console.log(continente.paises);
